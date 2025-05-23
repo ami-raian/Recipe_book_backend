bea99148e6b0ac0bb87dee626e57caffb35643fe
ami.raian <ami.robirai@gmail.com>
2025-05-24 02:01:41 +0600

    update fully on local and production only access

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const PORT = process.env.PORT || 5000;
 const MONGODB_URI = `${process.env.MONGODB_URI}`;
 
 // CORS SETUP
-app.use(cors());
+// app.use(cors());
+
 // app.use(
 //   cors({
 //     // origin: "http://localhost:5173", // Vite frontend
@@ -17,6 +18,28 @@ app.use(cors());
 //     allowedHeaders: ["Content-Type"],
 //   })
 // );
+
+const allowedOrigins = [
+  "http://localhost:5173", // Vite dev
+  "https://recipe-book-front-end.vercel.app", // Production
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true, // If you're using cookies or auth headers
+  })
+);
 
 // BODY PARSER
 app.use(express.json()); // built-in body parser
